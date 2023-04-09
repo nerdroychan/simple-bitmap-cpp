cb4df0218af94592e7f3a10b2b661b615dc9b74e
Chen Chen <mail@roychan.org>
2023-04-09 14:46:18 -0500

    update

--- bitmap_test.cpp
+++ bitmap_test.cpp
@@ -2,13 +2,15 @@
 
 #include "bitmap.h"
 
+#define SIZE (530941lu)
+
 int main()
 {
-    Bitmap b(255);
+    Bitmap b(SIZE);
 
     std::cout << "[all1 test]" << std::endl;
     b.all1();
-    for (uint64_t i=0; i<255; i++) {
+    for (uint64_t i=0; i<SIZE; i++) {
         if (!b.get(i)) {
             std::cout << i << " is not 1" << std::endl;
         }
@@ -16,14 +18,14 @@ int main()
 
     std::cout << "[all0 test]" << std::endl;
     b.all0();
-    for (uint64_t i=0; i<255; i++) {
+    for (uint64_t i=0; i<SIZE; i++) {
         if (b.get(i)) {
             std::cout << i << " is not 0" << std::endl;
         }
     }
 
     std::cout << "[set0/set1 test]" << std::endl;
-    for (uint64_t i=0; i<255; i++) {
+    for (uint64_t i=0; i<SIZE; i++) {
         if (i % 3 == 0) {
             b.set1(i);
         }
@@ -32,7 +34,7 @@ int main()
         }
     }
 
-    for (uint64_t i=0; i<255; i++) {
+    for (uint64_t i=0; i<SIZE; i++) {
         if (i % 3 == 0 && i % 9!= 0) {
             if (!b.get(i)) {
                 std::cout << i << " is not 1" << std::endl;
